23b9745bb94b18f7f21475e5ec704a8d6ad6d97b
Sjoerd Bolten <sjoerdabolten@gmail.com>
2023-01-17 21:37:38 +0100

    fix: fix build issues

--- src/components/base/Notification/state.ts
+++
@@ -1,4 +0,0 @@
-import { Ref, ref } from 'vue';
-import { NotificationOptions } from './api';
-
-export const notifications: Ref<NotificationOptions[]> = ref([]);
--- src/hooks/notification.ts
+++
@@ -1,5 +0,0 @@
-import * as notifications from '~/components/base/Notification/api';
-
-export const useNotifications = () => {
-  return notifications;
-};
